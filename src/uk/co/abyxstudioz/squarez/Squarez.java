9288be8c6eb50d5df920ab5d4624a0017e405c04
Laxynd <abyxcode@abyxstudioz.co.uk>
2013-11-20 14:23:03 +0000

    Random indentation was annoying me

--- src/uk/co/abyxstudioz/squarez/Squarez.java
+++ src/uk/co/abyxstudioz/squarez/Squarez.java
@@ -10,6 +10,7 @@ public class Squarez {
 	public static final String version = "0.5";
 	
 	public static void main(String args[]){
+		
    		frame.setSize(720, 480);
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
